2ff8bc37f6b0784b2fe0bdd78acd9bca54967589
Veronica <vecheza@gmail.com>
2018-05-21 01:22:41 -0300

    Clases e interfaces base

---
+++ Laboratorio 3/Laboratorio 3/Classes/Edificios/Almacenamiento/Castillo.cs
@@ -0,0 +1,13 @@
+﻿using Laboratorio_3.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3.Classes.Edificios.Almacenamiento
+{
+    public class Castillo : EdificioAlmacenamiento, IAlmacenamientoTropas, IAlmacenamientoNegro, IAlmacenamientoRojo
+    {
+    }
+}
---
+++ Laboratorio 3/Laboratorio 3/Classes/Tropas/Heroes/Rey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3.Classes.Tropas.Heroes
+{
+    public class Rey : Heroe
+    {
+    }
+}
